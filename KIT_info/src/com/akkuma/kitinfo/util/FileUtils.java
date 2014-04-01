720d7aa3b6e1e96c5d139b021d7e5f93944753d0
Atsuto Yamada <jayuiyong@gmail.com>
2014-04-01 18:57:02 +0900

    clean

--- KIT_info/src/com/akkuma/kitinfo/util/FileUtils.java
+++ KIT_info/src/com/akkuma/kitinfo/util/FileUtils.java
@@ -47,58 +47,6 @@ public class FileUtils {
         }
         return obj;
     }
-    
-//    @Deprecated
-//    public static void writeObjectToFile(Object object, String filename) {
-//        if (dontSave) {
-//            return;
-//        }
-//
-//        ObjectOutputStream objectOut = null;
-//        try {
-//
-//            FileOutputStream fileOut = new FileOutputStream(filename);
-//            objectOut = new ObjectOutputStream(fileOut);
-//            objectOut.writeObject(object);
-//            fileOut.getFD().sync();
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } finally {
-//            if (objectOut != null) {
-//                try {
-//                    objectOut.close();
-//                } catch (IOException e) {}
-//            }
-//        }
-//    }
-//
-//    @Deprecated
-//    public static Object readObjectFromFile(String filename) {
-//
-//        ObjectInputStream objectIn = null;
-//        Object object = null;
-//        try {
-//
-//            FileInputStream fileIn = new FileInputStream(filename);
-//            objectIn = new ObjectInputStream(fileIn);
-//            object = objectIn.readObject();
-//
-//        } catch (FileNotFoundException e) {
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        } finally {
-//            if (objectIn != null) {
-//                try {
-//                    objectIn.close();
-//                } catch (IOException e) {}
-//            }
-//        }
-//
-//        return object;
-//    }
 
     public static boolean isDontSave() {
         return dontSave;
